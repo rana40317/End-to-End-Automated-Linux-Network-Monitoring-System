3fceb6d4e0805aaa67f46ddfdb3e09464af97050
rana40317 <sayantankar1@gmail.com>
2026-07-17 22:14:43 +0530

    Version 2: End to End Automated Linux Network Monitoring System

--- network_monitor.sh
+++ network_monitor.sh
@@ -8,12 +8,18 @@ echo "***** Network Check : $(date) *****" > "$LOG_FILE"
 
 while read website port
 do
-	#Get ping Output
-	ping_output=$(ping -c 2 "$website" 2>/dev/null)
-
+	#Get 5 ping packets and save the  output
+	ping_output=$(ping -c 5 "$website" 2>/dev/null)
+        
+	#Check whether the ping command received any response
 	if [ $? -eq 0 ]
-	then
+	then 
+		#Extract average response time
 		latency=$(echo "$ping_output" | grep "rtt" | awk -F'/' '{print $5}' )
+                #Extract packet loss percentage
+		packet_loss=$(echo "$ping_output" | grep "packet loss" | awk -F ',' '{print $3}' | awk '{print $1}')
+		
+                #Check whether the configured TCP port is open
 
 	        if nc -zv  "$website" "$port"  > /dev/null 2>&1
 	        then
@@ -32,22 +38,6 @@ done < "$TARGET_FILE"
 echo "" >> "$LOG_FILE"
 
 cat "$LOG_FILE"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
